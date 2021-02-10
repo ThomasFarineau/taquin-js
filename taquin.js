76fa962ccdfb0e42d3c8ae43e16cd07246f82049
Arckene <32244230+ThomasFar06@users.noreply.github.com>
2021-02-10 17:12:37 +0100

    Patch possible way error

--- taquin.js
+++ taquin.js
@@ -1,10 +1,30 @@
+/**
+ *
+ * M413 - TD3 - Taquin Game
+ * *
+ * @author Jean-Michel Bruneau
+ *    @copyright UCA - IUT -INFO
+ * @version    1.0
+ * @date            2021-01-31
+ *
+ */
 "use strict";
 
+// M413 - Taquin
+let message = 'JavaScript is ok :)';
+// alert( message);
+console.log(message);
+
 function onLoad() {
-    generateGame(6) // Default game
+    console.log('Processus de chargement du document terminé…');
+    //
+    // All your JavaScript code goes here !
+    //
+    generateGame(4) // Default game
     init()
 }
 
+// Toute les ressources de la page sont complètement chargées.
 window.onload = onLoad;
 
 //OPTIMISED
@@ -18,38 +38,26 @@ function init() {
 //OPTIMISED
 function selection(event) {
     let target = (event.target.tagName === "SPAN") ? event.target.parentElement : event.target; //Si on clique sur le span on selectione l'id sinon l'id
-    let r = parseInt(target.id.substr(1)); // Numero de ligne
-    let c = parseInt(target.id.substr(r.toString().length + 3)); // Numero de colonne
-    const idToCheck = Array(4), colToCheck = Array(2), rowToCheck = Array(2);
-    //idToCheck prend la taille du nombre de déplacement maximum
+    let r = getRC(target.id)[0] // Numero de ligne
+    let c = getRC(target.id)[1] // Numero de colonne
+    let idToCheck; //idToCheck prend la taille du nombre de déplacement maximum
     let length = document.getElementsByClassName("box").length //Recuperation de la taille du tableau
-    if (r - 1 > 0) // Si on ne sort pas du tableau
-        rowToCheck.push(r - 1)
-    if (r + 1 <= Math.sqrt(length)) // Si on ne sort pas du tableau
-        rowToCheck.push(r + 1)
-
-    if (c - 1 > 0) // Si on ne sort pas du tableau
-        colToCheck.push(c - 1)
-    if (c + 1 <= Math.sqrt(length)) // Si on ne sort pas du tableau
-        colToCheck.push(c + 1)
-    //Ajout des coups possible selon la ligne
-    for (let rowToCheckElement of rowToCheck) {
-        if (!isNaN(rowToCheckElement)) idToCheck.push("r" + rowToCheckElement + "-c" + c)
-    }
-    //Ajout des coups possible selon la colonne
-    for (let colToCheckElement of colToCheck) {
-        if (!isNaN(colToCheckElement)) idToCheck.push("r" + r + "-c" + colToCheckElement)
-    }
+    idToCheck = nextTo(Math.sqrt(length), r, c)
     //Boucle qui permet de déplacer l'élément cliqué sur la place vide
     for (let idToCheckElement of idToCheck) {
         if (idToCheckElement !== undefined) { //On vérifie que l'élément du tableau est défini
+            idToCheckElement = toId(idToCheckElement[0], idToCheckElement[1])
             let dc = document.getElementById(idToCheckElement); //Récupération de l'élément via son id existant au préalable
             if (dc.classList.contains("empty")) { //Si l'élément a la classe empty
-                let content = target.innerHTML //On sauvegarde l'HTML de notre cible de base
-                target.innerHTML = "" //On vide le contenu HTML de notre cible
-                target.classList.add("empty") //On ajoute la classe empty à la cible
-                dc.classList.remove("empty") //On retire la classe empty a l'élément empty précède
-                dc.innerHTML = content //On lui met le contenu de la balise target sauvegardé dans content
+
+                let from = document.getElementById(target.id);
+                let to = document.getElementById(dc.id);
+                let content = from.innerHTML //On sauvegarde l'HTML de notre cible de base
+                from.innerHTML = "" //On vide le contenu HTML de notre cible
+                from.classList.add("empty") //On ajoute la classe empty à la cible
+                to.classList.remove("empty") //On retire la classe empty a l'élément empty précède
+                to.innerHTML = content //On lui met le contenu de la balise target sauvegardé dans content
+                console.log("Déplacement de " + from.id + " à " + to.id)
             }
         }
     }
@@ -87,16 +95,6 @@ Array.prototype.isSortedByValue = function () {
 }
 
 //OPTIMISED
-Array.prototype.shuffle = function () {
-    for (let i = this.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        const temp = this[i];
-        this[i] = this[j];
-        this[j] = temp;
-    }
-}
-
-//OPTIMISED
 function cheat() {
     let boxes = document.getElementsByClassName("box") // Recuperation des élements ".box"
         , value = [] // Tableau des valeurs
@@ -124,12 +122,53 @@ function generateGameInput(input) {
 }
 
 //OPTIMISED
+function getArrayId(size, r, c) {
+    return ((r - 1) * size + c) - 1
+}
+
+//OPTIMISED
+function getRC(id) {
+    let r = parseInt(id.substr(1)); // Numero de ligne
+    let c = parseInt(id.substr(r.toString().length + 3)); // Numero de colonne
+    return [r, c]
+}
+
+function toId(r, c) {
+    return "r" + r + "-c" + c
+}
+
+//OPTIMISED
+function nextTo(size, r, c) {
+    const idToCheck = [], colToCheck = [], rowToCheck = [];
+
+    if (r - 1 > 0) // Si on ne sort pas du tableau
+        rowToCheck.push(r - 1)
+    if (r + 1 <= size) // Si on ne sort pas du tableau
+        rowToCheck.push(r + 1)
+
+    if (c - 1 > 0) // Si on ne sort pas du tableau
+        colToCheck.push(c - 1)
+    if (c + 1 <= size) // Si on ne sort pas du tableau
+        colToCheck.push(c + 1)
+
+    //Ajout des coups possible selon la ligne
+    for (let rowToCheckElement of rowToCheck) {
+        if (!isNaN(rowToCheckElement)) idToCheck.push([rowToCheckElement, c])
+    }
+    //Ajout des coups possible selon la colonne
+    for (let colToCheckElement of colToCheck) {
+        if (!isNaN(colToCheckElement)) idToCheck.push([r, colToCheckElement])
+    }
+    return idToCheck;
+}
+
+//OPTIMISED
 function generateGame(size) {
-    let listSize = size * size - 1, value = []
-    for (let i = 0; i < listSize; i++) value.push(i + 1) // On attribue les valeurs a chaque element
-    value.shuffle() // On mélange les valeurs
     let game = document.getElementById("game")
     game.innerHTML = ""
+
+    let value = gameShuffle(size)
+
     for (let r = 1; r <= size; r++) {
         let row = document.createElement("div")
         row.classList.add("row")
@@ -137,13 +176,11 @@ function generateGame(size) {
             let div = document.createElement("div")
             div.setAttribute('id', 'r' + r + '-c' + c)
             div.classList.add('box')
-            let id = ((r - 1) * size + c) - 1 // Recuperation de la valeur du tableau par rapport au case
-            let value_with_id = value[id]
-            if (value[id] === undefined) {
+            if (value[getArrayId(size, r, c)] === "") {
                 div.classList.add('empty')
             } else {
                 let span = document.createElement("span")
-                let text = document.createTextNode(value_with_id)
+                let text = document.createTextNode(value[getArrayId(size, r, c)])
                 span.appendChild(text)
                 div.appendChild(span)
             }
@@ -154,3 +191,46 @@ function generateGame(size) {
     init()
 }
 
+function gameShuffle(size) {
+    let board = []
+    for (let i = 0; i < (size * size) - 1; i++) {
+        board.push(i + 1)
+    }
+    board.push("")
+    let multiplier = 250;
+    let empty = [parseInt(size), parseInt(size)]
+    for (let i = 0; i < size * multiplier; i++) {
+        let nextToEmpty = nextTo(size, empty[0], empty[1])
+        let chooseNext = nextToEmpty[getRandomInt(nextToEmpty.length)]
+        let currentID = getArrayId(size, empty[0], empty[1]);
+        let nextID = getArrayId(size, chooseNext[0], chooseNext[1]);
+        board[currentID] = board[nextID]
+        board[nextID] = ""
+        empty = [chooseNext[0], chooseNext[1]]
+    }
+
+
+    //Normalisation de la partie
+    while (board[board.length - 1] !== "") {
+        let nextR = empty[0] + 1;
+        let nextC = empty[1] + 1;
+        let chooseNext = empty
+        if (nextR <= size) {
+            chooseNext = [nextR, empty[1]]
+        }
+        if (nextC <= size) {
+            chooseNext = [empty[0], nextC]
+        }
+        let currentID = getArrayId(size, empty[0], empty[1]);
+        let nextID = getArrayId(size, chooseNext[0], chooseNext[1]);
+        board[currentID] = board[nextID]
+        board[nextID] = ""
+        empty = [chooseNext[0], chooseNext[1]]
+    }
+
+    return board
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
