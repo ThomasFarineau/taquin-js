a511c607c93b9d59ab1be3933dbf2100c41afbf9
Arckene <32244230+ThomasFar06@users.noreply.github.com>
2021-02-18 10:19:48 +0100

    Update taquin.js

--- taquin.js
+++ taquin.js
@@ -1,18 +1,17 @@
+"use strict";
+
 function onLoad() {
-    //generateGame(4) // Default game
-    init(4)
+    init(5)
 }
 
 let taquinGrid;
 
 //OPTIMISED
 function init(size) {
-    let values = gameShuffle(size)
-
     taquinGrid = Object.create(Grid)
     taquinGrid.size = size;
-    taquinGrid.values = values;
-
+    taquinGrid.values = gameShuffle();
+    console.log(taquinGrid)
     displayGrid()
 }
 
@@ -34,6 +33,7 @@ function getBoxes() {
     return document.querySelectorAll("div.box"); // Array des element ".box"
 }
 
+//OPTIMISED
 function displayGrid() {
     let game = document.querySelector("div#game")
     game.innerHTML = ""
@@ -45,11 +45,11 @@ function displayGrid() {
             let div = document.createElement("div")
             div.setAttribute('id', 'r' + r + '-c' + c)
             div.classList.add('box')
-            if (taquinGrid.values[getArrayId(taquinGrid.size, r, c)].value === "") {
+            if (taquinGrid.values[getArrayId(r, c)].value === "") {
                 div.classList.add('empty')
             } else {
                 let span = document.createElement("span")
-                let text = document.createTextNode(taquinGrid.values[getArrayId(taquinGrid.size, r, c)].value)
+                let text = document.createTextNode(taquinGrid.values[getArrayId(r, c)].value)
                 span.appendChild(text)
                 div.appendChild(span)
             }
@@ -57,6 +57,12 @@ function displayGrid() {
         }
         game.appendChild(row)
     }
+
+    if (taquinGrid.victoryState) {
+        document.querySelector("div#ifVictory").style.display = "block" // On affiche le modal "ifVictory"
+        document.querySelector("span#size").innerText = taquinGrid.size + "x" + taquinGrid.size // On écrit dans quelle était la taille de la partie
+    }
+
     let boxes = getBoxes();
     for (let i = 0; i < boxes.length; i++) {
         boxes[i].addEventListener("click", selection) //On ajout l'évènement click
@@ -70,67 +76,37 @@ function selection(event) {
 
     let idToCheck = selectCase.nextTo; //idToCheck récupère les cases autour de la selection
 
-    console.log(idToCheck)
     //Boucle qui permet de déplacer l'élément cliqué sur la place vide
     for (let idToCheckElement of idToCheck) {
         if (idToCheckElement !== undefined) { //On vérifie que l'élément du tableau est défini
-            idToCheckElement = toId(idToCheckElement[0], idToCheckElement[1])
-            let dc = document.querySelector("div#" + idToCheckElement); //Récupération de l'élément via son id existant au préalable
-            if (dc.classList.contains("empty")) { //Si l'élément a la classe empty
-                let from = document.querySelector("div#" + target.id);
-                let to = document.querySelector("div#" + dc.id);
-                let content = from.innerHTML //On sauvegarde l'HTML de notre cible de base
-                from.innerHTML = "" //On vide le contenu HTML de notre cible
-                from.classList.add("empty") //On ajoute la classe empty à la cible
-                to.classList.remove("empty") //On retire la classe empty a l'élément empty précède
-                to.innerHTML = content //On lui met le contenu de la balise target sauvegardé dans content
-                console.log("Déplacement de " + from.id + " à " + to.id)
+            let caseNextTo = getCaseByPos(idToCheckElement[0], idToCheckElement[1]) // Récupération de la case via ses coordonnée
+            if (caseNextTo.value === "") { //Si la case a coté est vide
+                changeValue(caseNextTo.r, caseNextTo.c, selectCase.value) // On change le contenue de la case vide
+                changeValue(selectCase.r, selectCase.c, "") // On vide la case cliqué
+                //console.log("Déplacement de " + selectCase + " à " + caseNextTo
             }
-
         }
     }
     checkVictory() //Verification si le coup est une victoire
+    displayGrid() // On recharge l'affichage
 }
 
 //OPTIMISED
 function checkVictory() {
-    let boxes = getBoxes() // Recuperation des élements ".box"
-        , value = []; // Tableau des valeurs
-    for (let i = 0; i < boxes.length; i++)
-        value.push(parseInt(boxes[i].innerText)) // On ajoute dans le tableau des valeurs chaque valeurs une a une
-    if (value.isSortedByValue()) { // Si les valeurs sont trier par valeur
-        document.querySelector("div#ifVictory").style.display = "block" // On affiche le modal "ifVictory"
-        document.querySelector("span#size").innerText = Math.sqrt(boxes.length) + "x" + Math.sqrt(boxes.length) // On écrit dans quelle était la taille de la partie
+    let valuesGrid = []
+    for (let i = 0; i < taquinGrid.values.length; i++) valuesGrid.push(taquinGrid.values[i].value)
+    console.log("vic", valuesGrid)
+    if (valuesGrid.isSortedByValue()) {
+        taquinGrid.victoryState = true
     }
 }
-
-function getCase(value) {
-    for (let i = 0; i < taquinGrid.values.length; i++) {
-        if (taquinGrid.values[i].value === value)
-            return taquinGrid.values[i]
-    }
-    return false
-}
-
 
 //OPTIMISED
 function cheat() {
-    let boxes = getBoxes() // Recuperation des élements ".box"
-        , value = [] // Tableau des valeurs
-
-    for (let i = 0; i < boxes.length; i++)
-        value.push(i + 1) // On ajoute les valeurs triée de 1 a boxes.length
-
-    for (let i = 0; i < boxes.length - 1; i++) {
-        boxes[i].innerHTML = "<span>" + value[i] + "</span>" // On met la nouvelle valeur dans la boxes numero i
-        boxes[i].classList.remove("empty") // On lui retire la classe empty au cas ou elle l'a (si la parti est en cours :))
-    }
-    let l = boxes.length
-        , content = boxes[l - 2].innerHTML //On sauvegarde l'HTML de notre cible de l'avant dernière case
-    boxes[l - 2].innerHTML = "" //On vide le contenu HTML de l'avant dernière case
-    boxes[l - 2].classList.add("empty") //On ajoute la classe empty à la avant dernière case
-    boxes[l - 1].classList.remove("empty") //On retire la classe empty a l'element ayant la classe empty
-    boxes[l - 1].innerHTML = content //On lui met le contenu de la balise sauvegardé dans content
+    for (let i = 0; i < taquinGrid.values.length; i++) taquinGrid.values[i].value = i + 1 // On met dans l'ordre les valeurs
+    taquinGrid.values[taquinGrid.values.length - 1].value = taquinGrid.values[taquinGrid.values.length - 2].value // On met la dernière valeur à l'avant dernière
+    taquinGrid.values[taquinGrid.values.length - 2].value = "" // On vide la valeur de l'avant dernière
+    displayGrid() // On affiche la grille
 }
 
 //OPTIMISED
@@ -138,28 +114,39 @@ function generateGameInput(input) {
     let size = document.querySelector("input#" + input).value
     init(size)
     document.querySelector("div#ifVictory").style.display = "none" // On affiche le modal "ifVictory"
-
 }
 
 //OPTIMISED
-function getArrayId(size, r, c) {
+function changeValue(r, c, value) {
+    for (let i = 0; i < taquinGrid.values.length; i++) if (taquinGrid.values[i].r === r && taquinGrid.values[i].c === c)
+        taquinGrid.values[i].value = value
+    return false
+}
+
+//OPTIMISED
+function getCase(value) {
+    for (let i = 0; i < taquinGrid.values.length; i++) if (taquinGrid.values[i].value === value)
+        return taquinGrid.values[i]
+    return false
+}
+
+//OPTIMISED
+function getCaseByPos(r, c) {
+    for (let i = 0; i < taquinGrid.values.length; i++) if (taquinGrid.values[i].r === r && taquinGrid.values[i].c === c)
+        return taquinGrid.values[i]
+    return false
+}
+
+
+//OPTIMISED
+function getArrayId(r, c) {
+    let size = taquinGrid.size;
     return ((r - 1) * size + c) - 1
 }
 
 //OPTIMISED
-function getRC(id) {
-    let r = parseInt(id.substr(1)); // Numero de ligne
-    let c = parseInt(id.substr(r.toString().length + 3)); // Numero de colonne
-    return [r, c]
-}
-
-//OPTIMISED
-function toId(r, c) {
-    return "r" + r + "-c" + c
-}
-
-//OPTIMISED
-function nextTo(size, r, c) {
+function nextTo(r, c) {
+    let size = taquinGrid.size;
     const idToCheck = [], colToCheck = [], rowToCheck = [];
 
     if (r - 1 > 0) // Si on ne sort pas du tableau
@@ -184,7 +171,9 @@ function nextTo(size, r, c) {
 }
 
 //OPTIMISED
-function gameShuffle(size) {
+function gameShuffle() {
+
+    let size = taquinGrid.size;
     let board = []
     for (let i = 0; i < (size * size) - 1; i++) {
         board.push(i + 1)
@@ -193,10 +182,10 @@ function gameShuffle(size) {
     let multiplier = 250;
     let empty = [parseInt(size), parseInt(size)]
     for (let i = 0; i < size * multiplier; i++) {
-        let nextToEmpty = nextTo(size, empty[0], empty[1])
+        let nextToEmpty = nextTo(empty[0], empty[1])
             , chooseNext = nextToEmpty[getRandomInt(nextToEmpty.length)]
-            , currentID = getArrayId(size, empty[0], empty[1])
-            , nextID = getArrayId(size, chooseNext[0], chooseNext[1]);
+            , currentID = getArrayId(empty[0], empty[1])
+            , nextID = getArrayId(chooseNext[0], chooseNext[1]);
         board[currentID] = board[nextID]
         board[nextID] = ""
         empty = [chooseNext[0], chooseNext[1]]
@@ -213,24 +202,23 @@ function gameShuffle(size) {
         if (nextC <= size) {
             chooseNext = [empty[0], nextC]
         }
-        let currentID = getArrayId(size, empty[0], empty[1]);
-        let nextID = getArrayId(size, chooseNext[0], chooseNext[1]);
+        let currentID = getArrayId(empty[0], empty[1]);
+        let nextID = getArrayId(chooseNext[0], chooseNext[1]);
         board[currentID] = board[nextID]
         board[nextID] = ""
         empty = [chooseNext[0], chooseNext[1]]
     }
-
     let caseBoard = []
-    for (let r = 1; r <= size; r++) {
-        for (let c = 1; c <= size; c++) {
-            let newCase = Object.create(Case)
-            newCase.r = r;
-            newCase.c = c;
-            newCase.nextTo = nextTo(4, r, c)
-            newCase.value = board[getArrayId(4, r, c)]
-            caseBoard.push(newCase)
-        }
+
+    for (let r = 1; r <= size; r++) for (let c = 1; c <= size; c++) {
+        let newCase = Object.create(Case)
+        newCase.r = r;
+        newCase.c = c;
+        newCase.nextTo = nextTo(r, c)
+        newCase.value = board[getArrayId(r, c)]
+        caseBoard.push(newCase)
     }
+
     return caseBoard
 }
 
@@ -251,9 +239,9 @@ Array.prototype.sortByValue = function () {
 //OPTIMISED
 Array.prototype.isSortedByValue = function () {
     let array = this.sortByValue()
-    for (let i = 0; i < this.length - 1; i++) { // On verifie pour chaque element de la chaine - 1 (le -1 est la valeur NaN)
-        if (this[i] !== array[i]) return false // Si il y a une différence alors ca retourne false
-    }
+    array.push(array.shift());
+    // On verifie pour chaque element de la chaine - 1 (le -1 est la valeur NaN)
+    for (let i = 0; i < this.length - 1; i++) if (this[i] !== array[i]) return false // Si il y a une différence alors ca retourne false
     return true; // Les arrays sont identique, on renvoie true
 }
 
